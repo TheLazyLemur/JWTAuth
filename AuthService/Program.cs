beea5ce376b7a527fa9c675ea3bc6948e378807d
Dan Rousseau <danrousseau@protonmail.com>
2020-11-24 09:12:30 +0200

    Added steeltoe endpointscore and added the health actuator

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Steeltoe.Management.Endpoint;
 
 namespace AuthService
 {
@@ -21,6 +22,6 @@ namespace AuthService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).AddHealthActuator();
     }
 }
